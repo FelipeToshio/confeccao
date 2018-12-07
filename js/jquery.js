a8103ec8e1fe3f1077526bb95f856783d3362d6b
Felipe Toshio <felipetoshiohikita@gmail.com>
2018-12-07 17:48:22 -0200

    V1.9

--- js/jquery.js
+++ js/jquery.js
@@ -1,32 +1,3 @@
-// document.onload = function () {
-    
-    
-
-    // for (i = 0; i < v.length; i++) {
-        
-        // v[i].addEventListener("click",
-            // function () {
-                // if (v[i].style.backgroundColor === "red") {
-                //     console.log("red");
-                //     v[i].style.backgroundColor = "yellow";
-                // } else if (v[i].style.backgroundColor === "yellow") {
-                //     console.log("yellow");
-                //     v[i].style.backgroundColor = "green";
-                // } else if (v[i].style.backgroundColor === "green") {
-                //     console.log("green");
-                //     v[i].style.backgroundColor = "rgb(255, 204, 102)";
-                // } else if (v[i].style.backgroundColor === "rgb(255, 204, 102)") {
-                //     console.log("blank");
-                //     v[i].style.backgroundColor = "red";
-                // }
-                // console.log("of");
-                // console.log("of");
-            // }
-            
-        // );
-       
-    // }
-// }
     var flag;
     function abreJanela(URL) {
         location.href = URL; // se for popup utiliza o window.open
@@ -52,7 +23,19 @@
         item.addEventListener('click', handleClick);
         item.style.backgroundColor = "red";
     });
+    
     function dados(flag){
         flag = !flag;
         document.getElementById("flag").style.display = "initial";
     }
+
+    var sit = document.querySelectorAll("td");
+    sit.forEach(function(status){
+        if(status.innerHTML === "Pronto"){
+            status.style.backgroundColor = "rgb(239,215,238)"
+        }
+        if(status.innerHTML === "Aberto"){
+            status.style.backgroundColor = "rgb(255,230,153)"
+        }
+    });
+   
